33a8e82009ebd983f986c626793a339d633cb506
Bitfeller <157190837+Bitfeller@users.noreply.github.com>
2024-01-01 00:00:00 +0000

    huge importing fixes

--- site/learn/createdeck.php
+++ site/learn/createdeck.php
@@ -102,10 +102,27 @@
                 <input type="checkbox" id="BI-replace-desc">
                 <span class="slider"></span>
             </label> | Use this imported deck's <b>description</b> in this deck<br>
-            Import your deck: <input type="file" id="BI-file" accept="text/plain"><br>
-            <button id="BI-createBtn">Import</button>
+            Import your deck (.json): <input type="file" id="BI-file" accept="application/json"><br>
+            <button id="BI-createBtn" disabled="true">Import</button>
             <p class="info-error" id="BI-err"></p>
         </div>
     </div>
+    <!-- Importing modal to show all questions about to be imported. TO BE IMPLEMENTED -->
+    <!-- <div class="import-modal" id="importing-modal" style="display: block;">
+        <div class="modal-content">
+            <h1>Importing:</h1>
+            <p>Below is every question about to be imported. Uncheck any you don't want.</p>
+            <button>Continue >>></button>
+            <button>Cancel</button><br>
+            <span class="material-symbols-outlined" id="qSelectAll">check_box_outline_blank</span>
+            <div id="importing-questions">
+                <div class="importing-question">
+                    <p class="q">What is the capital of France?</p>
+                    <p class="a">Paris</p>
+                    <input type="checkbox" checked>
+                </div>
+            </div>
+        </div>
+    </div> -->
 </body>
 </html>
