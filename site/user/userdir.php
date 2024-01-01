a2ef939475ffc7da41499b3059531225cfdd967a
IffyDeathValley99 <47678294+IffyDeathValley99@users.noreply.github.com>
2024-01-01 00:00:00 +0000

    CSS for password reset and stuff

--- site/user/userdir.php
+++ site/user/userdir.php
@@ -6,32 +6,57 @@
     <title>Bento!</title>
     <?php require_once "../globalreqs.php"?>
     <script type="module" src="../../sitejs/userdir.js" type="text/javascript" data-loading="true"></script>
-    <style>
-        section {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 90vh;
-        }
-        a {
-            font-size: 24pt;
-            margin-top: 1%;
-            padding: 0.5%;
-            border-radius: 10px;
-            background-color: #cadda0;
-            color:#1e1e1e;
-        }
-    </style>
 </head>
 <body>
     <?php require_once "../header.php"?>
-    <section class="holder">
-        <h1>Let's reset your password.</h1>
-        <p>Create a new password for your account.</p>
-        <input id="new-password" placeholder="New password...">
-        <button id="changepwd">Set as my password</button>
-        <p class="info-error"></p>
+    <section class="password-container">
+        <div class="holder"></div>
+        <div class="password-inputs">
+            <p class="info-blank">Enter a new password</p>
+            <input type="password" id="new-password" placeholder="New Password">
+            <button id="changepwd">Submit</button>
+        </div>
     </section>
 </body>
+<style>
+    .password-container h1, .password-container p {
+        text-align: center;
+    }
+    .password-container {
+        width: 700px;
+        background-color: var(--dark-gray);
+        padding: 15px;
+        border-radius: 10px;
+        min-height: 40dvh;
+        margin: auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        margin-top: 50px;
+        gap: 20px;
+    }
+    .password-container input {
+        text-align: center;
+    }
+    .password-inputs {
+        display: flex;
+        flex-direction: column;
+        min-width: 260px;
+        margin-top: 10px;
+        gap: 10px;
+        width: max-content;
+        margin: auto;
+        * {
+            font-size: 12pt;
+        }
+    }
+    #resetpwd {
+        padding: 0px 5px;
+    }
+    @media screen and (max-width: 700px) {
+        .password-container {
+            width: 90%;
+        }
+    }
+</style>
 </html>
