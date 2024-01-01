e6a9bdb70fde8fc4b9ace7cde3e6f7c40aa5c175
IffyDeathValley99 <47678294+IffyDeathValley99@users.noreply.github.com>
2024-01-01 00:00:00 +0000

    Host fonts locally
    Material design is not locally hosted however because it might be slower, not sure, code is commented.

--- site/globalreqs.php
+++ site/globalreqs.php
@@ -1,7 +1,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-<link href="https://fonts.googleapis.com/css2?family=Kadwa:wght@400;700&family=Karla:wght@200..800&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=add,add_a_photo,arrow_back_ios,arrow_forward_ios,bolt,box,check,check_indeterminate_small,chevron_right,close,delete,feedback,groups,indeterminate_check_box,lan,logout,notifications_off,refresh,remove,resume,rocket_launch,save,sentiment_very_satisfied,tune,visibility&display=block" />
 <link rel="stylesheet" href="/css/global.css"/>
 <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 <script type="module" src="../sitejs/client-modules/header.js"></script>
