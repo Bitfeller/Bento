c689b983243651f310c7de27f9cda2c54adc8172
Bitfeller <157190837+Bitfeller@users.noreply.github.com>
2024-01-01 00:00:00 +0000

    fix conflicts

--- server/client-gateway/user-gateway.js
+++ server/client-gateway/user-gateway.js
@@ -1,4 +1,4 @@
-import { types, sameUser, gateway_fetch } from './gateway-mod.js';
+import { types, sameUser, senderror, gateway_fetch } from './gateway-mod.js';
 
 const moduleurl = new URL(import.meta.url);
 const spath = moduleurl.pathname + "/../..";
@@ -13,7 +13,7 @@ let UserGateway = {
     getuser: async (getpfp = false, getudata = false, getreviews = true, getdrafts = false) => {
         if(!types("bbbb", getpfp, getudata, getreviews, getdrafts)) return [false, "invalid params"];
         if(!await sameUser()) return [false, "no session"];
-        let success = false, data = 'fetch-err';
+        let success = false, data = 'fetch-err', fres;
         await fetch(spath + "/php-db/user/user_get.php", {
             method: "post",
             headers: {
@@ -27,6 +27,7 @@ let UserGateway = {
             })
         }).then(res => {
             if(!res.ok) throw "couldn't fetch! (bad response)";
+            fres = res.clone();
             return res.json();
         }).then(res => {
             success = res.status == 'success';
@@ -35,12 +36,21 @@ let UserGateway = {
                 data = res.data;
                 if(data.userdata) data.userdata = JSON.parse(data.userdata);
             }
-        }).catch(e => console.log('backend:', e));
+        }).catch(async e => {
+            console.log('backend[user-gateway.js:getuser]:', e);
+            senderror("user-gateway.js:getuser", await fres.text(), {
+                plain: e,
+                getpfp,
+                getudata,
+                getreviews,
+                getdrafts
+            });
+        });
         return [success, data];
     },
     login: async (username, pwd) => {
         if(!types("SS", username, pwd)) return [false, "invalid params"];
-        let success = false, reason = 'fetch-err';
+        let success = false, reason = 'fetch-err', fres;
         await fetch(spath + "/php-db/user/user_login.php", {
             method: "post",
             headers: {
@@ -52,18 +62,24 @@ let UserGateway = {
             })
         }).then(res => {
             if(!res.ok) throw "couldn't fetch! (bad response)";
+            fres = res.clone();
             return res.json();
         }).then(res => {
             success = res.status == 'success', reason = '';
             if(!success) reason = res.reason;
-        }).catch(e => console.log('backend:', e));
+        }).catch(async e => {
+            console.log('backend[user-gateway.js:login]:', e);
+            senderror("user-gateway.js:login", await fres.text(), {
+                plain: e
+            });
+        });
         return [success, reason];
     },
     signup: async (username, pwd, email) => {
         if(!types("SSS", username, pwd, email)) return [false, "invalid params"];
         if(pwd.length < 8) return [false, "bad pwd"];
         if(await isCommon(pwd)) return [false, "bad pwd"];
-        let success = false, reason = 'fetch-err';
+        let success = false, reason = 'fetch-err', fres;
         await fetch(spath + "/php-db/user/user_new.php", {
             method: 'post',
             headers: {
@@ -76,17 +92,23 @@ let UserGateway = {
             })
         }).then(res => {
             if(!res.ok) throw "couldn't fetch! (bad response)";
+            fres = res.clone();
             return res.json();
         }).then(res => {
             success = res.status == 'success', reason = '';
             if(!success) reason = res.reason;
-        }).catch(e => console.log('backend:', e));
+        }).catch(async e => {
+            console.log('backend[user-gateway.js:signup]:', e);
+            senderror("user-gateway.js:signup", await fres.text(), {
+                plain: e
+            });
+        });
         return [success, reason];
     },
     editUser: async (setting, val, pwd = "") => {
         if(!types("Sss", setting, val, pwd)) return [false, "invalid params"];
         if(!await sameUser()) return [false, "no session"];
-        let success = false, reason = 'fetch-err';
+        let success = false, reason = 'fetch-err', fres;
         await fetch(spath + "/php-db/user/user_edit.php", {
             method: 'post',
             headers: {
@@ -99,11 +121,18 @@ let UserGateway = {
             })
         }).then(res => {
             if(!res.ok) throw "couldn't fetch! (bad response)";
+            fres = res.clone();
             return res.json();
         }).then(res => {
             success = res.status == 'success', reason = '';
             if(!success) reason = res.reason;
-        }).catch(e => console.log('backend:', e));
+        }).catch(async e => {
+            console.log('backend[user-gateway.js:editUser]:', e);
+            senderror("user-gateway.js:editUser", await fres.text(), {
+                plain: e,
+                setting
+            });
+        });
         return [success, reason];
     },
     signout: async () => {
@@ -118,7 +147,7 @@ let UserGateway = {
     getDraftImage: async time => {
         if(!types("n", time)) return [false, "invalid params"];
         if(!await sameUser()) return [false, "no session"];
-        let success = false, data = 'fetch-err';
+        let success = false, data = 'fetch-err', fres;
         await fetch(spath + "/php-db/user/user_draft_getpic.php", {
             method: 'post',
             headers: {
@@ -129,18 +158,25 @@ let UserGateway = {
             })
         }).then(res => {
             if(!res.ok) throw "couldn't fetch! (bad response)";
+            fres = res.clone();
             return res.json();
         }).then(res => {
             success = res.status == 'success';
             if(!success) data = res.reason;
             else data = res.data;
-        }).catch(e => console.log('backend:', e));
+        }).catch(async e => {
+            console.log('backend[user-gateway.js:getDraftImage]:', e);
+            senderror("user-gateway.js:getDraftImage", await fres.text(), {
+                plain: e,
+                time
+            });
+        });
         return [success, data];
     },
     giveFeedback: async feedback => {
         if(!types("S", feedback)) return [false, "invalid params"];
         if(!await sameUser()) return [false, "no session"];
-        let success = false, reason = 'fetch-err';
+        let success = false, reason = 'fetch-err', fres;
         await fetch(spath + "/php-db/feedback/feedback_new.php", {
             method: 'post',
             headers: {
@@ -151,11 +187,18 @@ let UserGateway = {
             })
         }).then(res => {
             if(!res.ok) throw "couldn't fetch! (bad response)";
+            fres = res.clone();
             return res.json();
         }).then(res => {
             success = res.status == 'success', reason = '';
             if(!success) reason = res.reason;
-        }).catch(e => console.log('backend:', e));
+        }).catch(async e => {
+            console.log('backend[user-gateway.js:giveFeedback]:', e);
+            senderror("user-gateway.js:giveFeedback", await fres.text(), {
+                plain: e,
+                feedback
+            });
+        });
         return [success, reason];
     },
     calculateDays: (box) => {
@@ -176,7 +219,7 @@ let UserGateway = {
     // Recovery and verification methods
     userdir: async (mode, uid, verif, newPwd = "") => {
         if(!types("Sns", mode, uid, verif)) return [false, "invalid params"];
-        let success = false, reason = 'fetch-err';
+        let success = false, reason = 'fetch-err', fres;
         await fetch(spath + "/php-db/user/user_direct.php", {
             method: 'post',
             headers: {
@@ -190,16 +233,23 @@ let UserGateway = {
             })
         }).then(res => {
             if(!res.ok) throw "couldn't fetch! (bad response)";
+            fres = res.clone();
             return res.json();
         }).then(res => {
             success = res.status == 'success', reason = '';
             if(!success) reason = res.reason;
-        }).catch(e => console.log('backend:', e));
+        }).catch(async e => {
+            console.log('backend[user-gateway.js:userdir]:', e);
+            senderror("user-gateway.js:userdir", await fres.text(), {
+                plain: e,
+                mode
+            });
+        });
         return [success, reason];
     },
     resetPwd: async email => {
         if(!types("s", email)) return [false, "invalid params"];
-        let success = false, reason = 'fetch-err';
+        let success = false, reason = 'fetch-err', fres;
         await fetch(spath + "/php-db/user/user_resetpwd.php", {
             method: 'post',
             headers: {
@@ -210,11 +260,17 @@ let UserGateway = {
             })
         }).then(res => {
             if(!res.ok) throw "couldn't fetch! (bad response)";
+            fres = res.clone();
             return res.json();
         }).then(res => {
             success = res.status == 'success', reason = '';
             if(!success) reason = res.reason;
-        }).catch(e => console.log('backend:', e));
+        }).catch(async e => {
+            console.log('backend[user-gateway.js:resetPwd]:', e);
+            senderror("user-gateway.js:resetPwd", await fres.text(), {
+                plain: e
+            });
+        });
         return [success, reason];
     }
 }
