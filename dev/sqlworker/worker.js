c7b9702fbfb2b4930e90b57c57aacea7d6e159d7
Bitfeller <157190837+Bitfeller@users.noreply.github.com>
2024-01-01 00:00:00 +0000

    sql worker

---
+++ dev/sqlworker/worker.js
@@ -0,0 +1,32 @@
+// SQLWorker utilized for updating db for v0.4.0
+
+const { connect, query } = require('./sql.js');
+
+(async () => {
+    const conn = await connect();
+    const users = await query("SELECT * FROM users");
+    let updated = 0;
+    for(let row of users) {
+        let last = row.userdata;
+        row.userdata = row.userdata.replaceAll('\\n', '').replaceAll('\\r', '');
+        if(last != row.userdata) {
+            updated++;
+            await query("UPDATE users SET userdata = '" + row.userdata + "' WHERE id = " + row.id);
+        }
+    }
+    console.log(`Updated ${updated} users.`);
+    updated = 0;
+
+    const decks = await query("SELECT * FROM decks");
+    for(let row of decks) {
+        let last = row.data;
+        row.data = row.data.replaceAll('\\n', '').replaceAll('\\r', '');
+        if(last != row.data) {
+            updated++;
+            await query("UPDATE decks SET data = '" + row.data + "' WHERE id = " + row.id);
+        }
+    }
+    console.log(`Updated ${updated} decks.`);
+
+    conn.end();
+})();
