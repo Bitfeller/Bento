f1043a017f016fc89fc13d528675978385877288
IffyDeathValley99 <47678294+Va1ley@users.noreply.github.com>
2024-01-01 00:00:00 +0000

    fix flush button on create deck

--- site/learn/createdeck.php
+++ site/learn/createdeck.php
@@ -29,7 +29,9 @@
     <div class="container">
         <div class="create-container">
             <h1>Create a Deck</h1>
-            <button id="flush">Flush Deck</button>
+            <button id="flush" class="line-up-icons">
+                <span class="material-symbols-outlined" id="picAddBtn">delete</span>
+            </button>
             <input type="text" placeholder="Name" id="name">
             <input type="text" placeholder="Description" id="description">
             <p>Deck picture:</p>
