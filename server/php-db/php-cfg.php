d24f8545342cccea267953d79b9018f56612c134
Bitfeller <157190837+Bitfeller@users.noreply.github.com>
2024-01-01 00:00:00 +0000

    fixed critical bugs

--- server/php-db/php-cfg.php
+++ server/php-db/php-cfg.php
@@ -1,5 +1,9 @@
 <?php
-    $PHP_CONFIG = [
-        'json_max_depth' => 512,
-        'json_flags' => JSON_THROW_ON_ERROR
-    ];
+    global $PHP_CONFIG;
+    function GET_PHP_CONFIG() {
+        $PHP_CONFIG = [
+            'json_max_depth' => 512,
+            'json_flags' => JSON_THROW_ON_ERROR
+        ];
+        return $PHP_CONFIG;
+    }
