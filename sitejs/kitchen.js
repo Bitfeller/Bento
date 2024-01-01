4d0be7deebb70c250959e43c154c9adc1546b7e7
Bitfeller <157190837+Bitfeller@users.noreply.github.com>
2024-01-01 00:00:00 +0000

    bug fix

--- sitejs/kitchen.js
+++ sitejs/kitchen.js
@@ -380,7 +380,13 @@ async function fetchDecks(start = 0) {
         hasMtch.checked
     );
     
-    if(!success) return;
+    if(!success) 
+        return 0;
+
+    // Update "Show More Results" btn
+    loadMore.innerHTML = "Show More Results";
+    loadMore.style.pointerEvents = "all";
+    loadMore.style.opacity = "1";
 
     if(start > 0) {
         decks.push(...data);
@@ -391,6 +397,7 @@ async function fetchDecks(start = 0) {
     }
 
     await update();
+    return data.length;
 }
 
 // Color map
