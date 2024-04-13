4a73277e1d532ca9160d44dc1362d6e755dc0d86
IffyDeathValley99 <47678294+IffyDeathValley99@users.noreply.github.com>
2024-04-12 21:39:29 -0600

    change to php

---
+++ html/header.php
@@ -0,0 +1,7 @@
+<header class=header>
+  <img alt="Bento! Logo" class=logo height=40px onclick='location.href="/html/home.html"' src="/img/bento logo white.svg">
+  <div class=right-header>
+    <span class="header-nav material-symbols-outlined">account_circle</span>
+    <span class="header-nav material-symbols-outlined">tune</span>
+  </div>
+</header>
