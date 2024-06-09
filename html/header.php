2fda88e61f580eeae8f1caaf60576fc256ba39d2
Bitfeller <ashusv@outlook.com>
2024-06-09 11:33:44 -0600

    improvements

--- html/header.php
+++ html/header.php
@@ -1,6 +1,8 @@
 <header class=header>
+  <script type="module" src="../js/header.js"></script>
   <img alt="Bento! Logo" class=logo height=40px onclick='location.href="/home"' src="/img/bento logo white.svg">
   <div class=right-header>
+    <span class="header-nav material-symbols-outlined" id="logout">logout</span>
     <span class="header-nav material-symbols-outlined">account_circle</span>
     <span class="header-nav material-symbols-outlined" onclick='location.href="/settings"'>tune</span>
   </div>
