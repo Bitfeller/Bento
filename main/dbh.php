ab46fca383eecc4c01813e87c5590c8ae6813f27
Bitfeller <157190837+Bitfeller@users.noreply.github.com>
2024-06-09 20:42:21 -0600

    edits

--- main/dbh.php
+++ main/dbh.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'funcs.php';
-    $conn = mysqli_connect("localhost", "remote", "", "bento");
+    $conn = mysqli_connect("10.10.10.210", "remote", "*7ED54C88139248C900757D5540148B5AA2DBF4F2", "bento");
     if(!$conn) {
         fail("conn: ". mysqli_connect_error());
     }
