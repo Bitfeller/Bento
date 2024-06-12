7cf4d98ea80a36941b2917b2bc6639e0da59829a
Bitfeller <157190837+Bitfeller@users.noreply.github.com>
2024-06-12 17:27:09 -0600

    more changes

--- site/404.php
+++ site/404.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bento - 404!</title>
-    <?php require_once "globalReqs.php"?>
+    <?php require_once "globalreqs.php"?>
 </head>
 <style>
     section {
--- site/learn/learnpicker.php
+++ site/learn/learnpicker.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Learn Picker</title>
-    <?php require_once "../globalReqs.php"?>
+    <?php require_once "../globalreqs.php"?>
     <link rel="stylesheet" href="../../css/learnPicker.css"/>
     <script type="module" src="../../sitejs/learnpicker.js"> </script>
 </head>
