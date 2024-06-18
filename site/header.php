4e91404bd29cb837d5b0c5f3754d585375326c8d
IffyDeathValley99 <47678294+IffyDeathValley99@users.noreply.github.com>
2024-06-18 17:06:58 -0600

    profile page + more mobile friendly

--- site/header.php
+++ site/header.php
@@ -2,7 +2,7 @@
   <script type="module" src="../sitejs/header.js"></script>
   <img alt="Bento! Logo" class=logo height=40px onclick='location.href="/home"' src="/img/bento logo white.svg">
   <div class=right-header>
-    <img class="pfp right-header-ico" src="/img/default-pfp.png">
+    <img class="pfp right-header-ico" src="/img/default-pfp.png" onclick='location.href="/user/profile"'>
     <span class="header-nav material-symbols-outlined right-header-ico" onclick='location.href="/user/settings"'>tune</span>
     <span class="header-nav material-symbols-outlined right-header-ico" id="header:logout">logout</span>
   </div>
