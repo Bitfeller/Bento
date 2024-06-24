b367ffdac108913154e1be3468aa22d72ae4220e
Bitfeller <ashusv@outlook.com>
2024-06-23 19:45:22 -0600

    better stuff

--- site/maintenance.php
+++ site/maintenance.php
@@ -5,42 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bento! | Maintenance</title>
     <?php require_once "globalreqs.php"?>
-    <style>
-    section {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 90vh;
-    }
-    a {
-        font-size: 24pt;
-        margin-top: 1%;
-        padding: 0.5%;
-        border-radius: 10px;
-        background-color: #cadda0;
-        color:#1e1e1e;
-    }
-    #a {
-        position: absolute;
-        height: 540px;
-        left: calc(50% - 340px);
-        bottom: 50%;
-        content:url("/img/menhera-sad.gif");
-        animation: ability-to-use 600s;
-    }
-        @keyframes ability-to-use {
-        0% {opacity: 0;}
-        10% {opacity: 0;}
-        100% {opacity: 1;}
-    }
-    </style>
+    <link rel="stylesheet" href="/css/index.css"/>
+    <script>
+        // Code to update time expectancy for when Bento will be back up
+        // still has to be implemented...
+    </script>
 </head>
 <body>
-    <?php require_once "header.php" ?>
-    <section id="404-content"> 
+    <header>
+        <div class="left-header">
+            <img id="logo" alt="Bento! Logo" src="/img/bento logo white.svg" height="40px" onclick="location.href='/home'">
+        </div>
+    </header>
+    <section id="hero">
+        <div id="hero-elements">
+            <h1>
+                We're making Bento <b>better</b>.
+            </h1>
+            <h2>
+                Bento's currently under maintenance, but we'll be back up shortly. Check again later.
+            </h2>
+            <p>We should be back up <b>in 5 minutes.</b></p>
+        </div>
+    </section>
+    <!--<section id="404-content"> 
         <h2>Uh oh! Looks like the site is under maintenance. Please check back later!</h2>
     </section>
-    <img id="a">
+    <img id="a">-->
 </body>
 </html>
