669551c6510f026cd25bd09ac53c14529f75c2a6
Bitfeller <ashusv@outlook.com>
2024-08-13 10:55:28 -0600

    notif service fix + other small fixes

--- sitejs/client-modules/service-worker.js
+++ sitejs/client-modules/service-worker.js
@@ -42,7 +42,7 @@ self.addEventListener("activate", init);
 self.addEventListener("push", async e => {
     const data = await e.data.json();
     const regis = self.registration;
-    if(data.type == "reviewnotifcheck" && user) {
+    if(data.type == "reviewnotifcheck") {
         // Check if the user has blocked notifications; if so, exit.
         if(Notification.permission != "granted") {
             sub.unsubscribe();
@@ -60,56 +60,15 @@ self.addEventListener("push", async e => {
             await regis.unregister();
             return;
         }
-        // Update user's info
-        let [success, _user] = await UserGateway.getuser();
-        if(!success) return;
-        user = _user;
-        let deckcount = 0;
-        let reviews = user.reviews;
-        let r_keys = Object.keys(reviews);
-        for(let i = 0; i < r_keys.length; i++) {
-            let [success, deck] = await DeckGateway.get(parseInt(r_keys[i]));
-            if(!success) continue;
-            let count = 0;
-            let c_keys = Object.keys(reviews[r_keys[i]]);
-            for(let j = 0; j < c_keys.length; j++) {
-                let term = reviews[r_keys[i]][c_keys[j]];
-                if(UserGateway.calculateNTR(term.box, term.last)) count++;
-            }
-            count += Object.keys(deck.data.contnt).length - c_keys.length;
-            if(count > 0) deckcount++;
-        }
-        elapsedDays += 1;
-        if(deckcount == 0) return;
-        switch(user.notifsub) {
-            case "0":
-                sub.unsubscribe();
-                fetch("https://bentoapi.valleynas.uk:443/notify", {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        unsubscribe: true
-                    })
-                }).catch(async err => {
-                    console.error("failed to unsubscribe to push notifs; reason:", err);
-                });
-                await regis.unregister();
-                return;
-            case "2":
-                if(elapsedDays % 3 != 0) return;
-            break;
-            case "3":
-                if(elapsedDays % 7 != 0) return;
-            break;
-        }
         const notif = regis.showNotification("Bento | Time to review.", {
-            body: `You currently have ${deckcount} deck${deckcount == 1 ? "" : "s"} to review.`,
+            body: `You currently have ${data.deckcount} deck${data.deckcount == 1 ? "" : "s"} to review.`,
             tag: 'reviewnotif-' + Date.now()
         });
         notif.onclick = () => {
             open("https://bento.valleynas.uk/home");
         };
+    } else if(data.type == "unsubscribe") {
+        sub.unsubscribe();
+        await regis.unregister();
     }
 });
