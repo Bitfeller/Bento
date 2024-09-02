c7a8eb32674ddd5e4c8659664bbc74ea0c1e1bad
Bitfeller <ashusv@outlook.com>
2024-09-01 22:18:41 -0600

    implemented themes CORRECTLY

--- site/globalreqs.php
+++ site/globalreqs.php
@@ -5,14 +5,3 @@
 <link rel="stylesheet" href="/css/global.css"/>
 <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 <script type="module" src="../sitejs/client-modules/header.js"></script>
-<?php 
-    $theme = 'nord';
-    if ($theme === 'nord') {
-        return;
-    } elseif ($theme === 'coffee-midnight') {
-        echo '<link rel="stylesheet" href="/css/themes/coffee-midnight.css">';
-    } else if ($theme === 'catppuccin') {
-        echo '<link rel="stylesheet" href="/css/themes/catppuccin.css">';
-    } else if ($theme === 'classic') {
-        echo '<link rel="stylesheet" href="/css/themes/classic.css">';
-    }
