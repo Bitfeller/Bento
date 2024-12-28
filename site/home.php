f92a42dc663f901160f6901a7e4dac2f90746f87
Bitfeller <ashusv@outlook.com>
2024-12-27 17:12:01 -0700

    matching functionality in createdeck

--- site/home.php
+++ site/home.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bento - Home</title>
     <?php require_once "globalreqs.php"?>
-    <link rel="stylesheet" href="/css/home2.css"/>
+    <link rel="stylesheet" href="/css/home.css"/>
     <script type="module" src="../sitejs/home.js" data-loading="true"></script>
 </head>
 <body data-uo="true">
