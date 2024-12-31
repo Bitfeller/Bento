08839a242ad5e95052625e4557615e6a412fc6e2
IffyDeathValley99 <47678294+IffyDeathValley99@users.noreply.github.com>
2024-12-31 08:01:29 -0700

    Updated profile page, fixed some other issues

--- site/globalreqs.php
+++ site/globalreqs.php
@@ -1,6 +1,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=add,add_a_photo,arrow_back_ios,arrow_forward_ios,bolt,box,check,check_box_check_box_outline_blank,check_indeterminate_small,chevron_right,close,delete,feedback,groups,indeterminate_check_box,info,lan,logout,notifications_off,refresh,remove,resume,rocket_launch,save,sentiment_very_satisfied,tune,visibility&display=block" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=add,add_a_photo,arrow_back_ios,arrow_forward_ios,bolt,box,check,check_box_check_box_outline_blank,check_indeterminate_small,chevron_right,close,delete,download,edit,feedback,groups,indeterminate_check_box,info,lan,logout,notifications_off,refresh,remove,resume,rocket_launch,save,sentiment_very_satisfied,tune,visibility&display=block" />
 <link rel="stylesheet" href="/css/global.css"/>
 <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 <script type="module" src="../sitejs/client-modules/header.js"></script>
