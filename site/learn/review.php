b1cc08fb29913e93015c0cb0e4e25da2e726192e
IffyDeathValley99 <47678294+Va1ley@users.noreply.github.com>
2025-05-26 16:43:26 -0600

    Initial review page structure and text styling

---
+++ site/learn/review.php
@@ -0,0 +1,102 @@
+<?php $_X_UO = true; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bento | Learn</title>
+    <link rel="stylesheet" href="../../css/review.css"/>
+    <?php require_once "../globalreqs.php"?>
+    <script type="module" src="../../sitejs/review.js" data-loading="true"></script>
+    <!-- MathJax -->
+    <script>
+        window.MathJax = {
+            tex: {
+                inlineMath: [['$', '$'], ['\\(', '\\)']],
+                displayMath: []
+            },
+            svg: {
+                fontCache: 'global',
+                scale: 1,
+            },
+            startup: {}
+        };
+    </script>
+    <script type="text/javascript" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"></script>
+</head>
+<body>
+    <?php require_once "../header.php"?>
+    <div class="progress-display">
+        <div id="progressBar"></div>
+        <div class="progress-icons">
+            <span class="line-up-icons">
+                <span id="percentageCorrect">0</span>
+                <span class="material-symbols-outlined">percent</span>
+            </span>
+            <span class="line-up-icons">
+                <span id="numberCompleted">0</span>
+                <span class="material-symbols-outlined">check</span>
+            </span>
+            <span class="line-up-icons">
+                <span id="numberRemaining">0</span>
+                <span class="material-symbols-outlined">box</span>
+            </span>
+        </div>
+    </div>
+    <div class="review-container">
+        <div id="question">
+            <!-- Just text -->
+            <span id="questionText">Which epic poem depicts Satan, Beelzebub, and o</span>
+
+            <!-- Text and picture -->
+            <!-- <span id="questionText"></span>
+            <img id="questionImage" src="" alt="Question Image" style="display: none;"> -->
+            </div>
+        <div id="answers">
+            <!-- Text -->
+            <input type="text" id="answerText" placeholder="Type your answer here..." autocomplete="off">
+
+            <!-- Multiple Choice -->
+            <!-- <div class="mc-option"></div>
+            <div class="mc-option"></div>
+            <div class="mc-option"></div>
+            <div class="mc-option"></div> -->
+
+            <!-- Ranking -->
+            <!-- <div class="ranking-option"></div>
+            <div class="ranking-option"></div>
+            <div class="ranking-option"></div> -->
+
+            <!-- Matching -->
+            <!-- <div class="matching-option"></div>
+            <div class="matching-option"></div>
+            <div class="matching-option"></div> -->
+
+            <!-- More?! -->
+            <div class="answer-options">
+                <span id="reshow">
+                    Show Question Again <span class="shortcut">R</span>
+                </span>
+                <span id="markCorrect">
+                    Mark Answer as Correct <span class="shortcut">_</span>
+                </span>
+            </div>
+        </div>
+        <!-- 
+            Multiple choice:
+                right: option chose appears correct
+                wrong: option chose appears incorrect, correct option appears correct
+            Ranking:
+                right: all answers briefly flash correct
+                wrong: answers rearrange themselves to correct order, those in incorrect order are marked incorrect, others correct
+            Matching:
+                right: all answers briefly flash correct
+                wrong: correct answers appear correct, incorect answers show selected option in incorrect and correct together
+            Text:
+                right: answer appears correct 
+                wrong: the correct answer appears below
+                
+        -->
+
+    </div>
+</body>
